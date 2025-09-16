d7693731870b35b8626f1c9a041c4d6bb7b36169
Jay Depani <work.depani@gmail.com>
2025-09-16 09:56:30 +0530

    :art: fix bug

--- src/components/navigation/drawer.tsx
+++ src/components/navigation/drawer.tsx
@@ -1,8 +1,8 @@
-import * as React from 'react';
-import * as DrawerPrimitive from '@radix-ui/react-dialog';
-import { cva, type VariantProps } from 'class-variance-authority';
+import * as React from "react";
+import * as DrawerPrimitive from "@radix-ui/react-dialog";
+import { cva, type VariantProps } from "class-variance-authority";
 
-import { mergeClasses } from '@/lib/utils';
+import { mergeClasses } from "@/lib/utils";
 
 const Drawer = DrawerPrimitive.Root;
 
@@ -24,7 +24,7 @@ const DrawerOverlay = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <DrawerPrimitive.Overlay
     className={mergeClasses(
-      'fixed inset-0 z-50 bg-gray-900/10 opacity-100 backdrop-blur-sm',
+      "fixed inset-0 z-50 bg-gray-900/10 opacity-100 backdrop-blur-sm",
       className
     )}
     {...props}
@@ -34,16 +34,16 @@ const DrawerOverlay = React.forwardRef<
 DrawerOverlay.displayName = DrawerPrimitive.Overlay.displayName;
 
 const drawerVariants = cva(
-  'fixed z-50 shadow-2xl bg-gray ring-1 ring-black/10 transition-all ease-in-out duration-100',
+  "fixed z-50 shadow-2xl bg-gray ring-1 ring-black/10 transition-all ease-in-out duration-100",
   {
     variants: {
       side: {
         right:
-          'inset-y-0 right-0 h-full max-w-xs w-full data-[state=open]:animate-drawer-open data-[state=closed]:animate-drawer-close',
+          "inset-y-0 right-0 h-full max-w-xs w-full data-[state=open]:animate-drawer-open data-[state=closed]:animate-drawer-close",
       },
     },
     defaultVariants: {
-      side: 'right',
+      side: "right",
     },
   }
 );
@@ -55,7 +55,7 @@ interface DrawerContentProps
 const DrawerContent = React.forwardRef<
   React.ElementRef<typeof DrawerPrimitive.Content>,
   DrawerContentProps
->(({ side = 'right', className, children, ...props }, ref) => (
+>(({ side = "right", className, children, ...props }, ref) => (
   <DrawerPortal>
     <DrawerOverlay />
     <DrawerPrimitive.Content
